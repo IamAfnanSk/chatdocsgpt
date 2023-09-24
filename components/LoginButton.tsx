4e8c60337ae43c88f070d60cdd5cce7b10eadfa1
Afnan Shaikh <48408572+IamAfnanSk@users.noreply.github.com>
2023-09-24 14:31:43 +0530

    basic setup with auth

---
+++ components/LoginButton.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { Mail } from "lucide-react";
+import toast from "react-hot-toast";
+
+export function LoginButton() {
+  const supabase = createClientComponentClient();
+
+  const handleSignIn = async () => {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: `${location.origin}/auth/callback`,
+      },
+    });
+
+    if (error) {
+      toast.error(error.message);
+    }
+  };
+
+  return (
+    <Button onClick={handleSignIn}>
+      <Mail className="mr-2 h-4 w-4" /> Login with Google
+    </Button>
+  );
+}
--- components/LogoutButton.tsx
+++ components/LogoutButton.tsx
@@ -1,9 +1,9 @@
-export default function LogoutButton() {
+import { Button } from "@/components/ui/button";
+
+export function LogoutButton() {
   return (
     <form action="/auth/sign-out" method="post">
-      <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
-        Logout
-      </button>
+      <Button>Logout</Button>
     </form>
-  )
+  );
 }
